b703e1e9edb4c482f1f215d6475ff0cbb236503b
Andre Pirjanian <pirjanianandre@gmail.com>
2023-02-27 11:23:41 -0800

    comment batch writing successful

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,10 +1,12 @@
 import { Post } from "@/src/atoms/postsAtom";
 import About from "@/src/components/community/About";
 import PageContent from "@/src/components/layout/PageContent";
+import Comments from "@/src/components/posts/comments/Comments";
 import PostItem from "@/src/components/posts/PostItem";
 import { auth, firestore } from "@/src/firebase/clientApp";
 import useCommunityData from "@/src/hooks/useCommunityData";
 import usePosts from "@/src/hooks/usePosts";
+import { User } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { userInfo } from "os";
@@ -55,7 +57,11 @@ const PostPage: React.FC = () => {
             userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
           />
         )}
-        {/* Comments */}
+        <Comments
+          user={user as User}
+          selectedPost={postStateValue.selectedPost}
+          communityId={postStateValue.selectedPost?.communityId as string}
+        />
       </>
       <>
         {communityStateValue.currentCommunity && (
